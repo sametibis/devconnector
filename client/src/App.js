778ec97a07494856d07396e377781d3f3c2f3d95
sametibis <sametibs.dev@gmail.com>
2020-08-28 23:36:42 +0300

    Profile Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utilities/setAuthToken';
 import './App.css';
@@ -12,6 +13,7 @@ import './App.css';
 // Redux;
 import { Provider } from 'react-redux'; // connect react and redux
 import store from './store';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -33,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
