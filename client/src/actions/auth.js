cb2f2bacb94ac76c75f6ec49eb6173d6c8d06e65
sametibis <sametibs.dev@gmail.com>
2020-08-28 15:44:49 +0300

    Logout

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from '../utilities/setAuthToken';
 
@@ -92,3 +93,10 @@ export const login = ( email, password ) => async (dispatch) => {
     })
   }
 };
+
+// Logout
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  })
+}
